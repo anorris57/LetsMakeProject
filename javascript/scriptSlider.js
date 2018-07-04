1be9a7af431ea3c7296cb37b3a85780287e7dd54
Mathieu Nevins <mnevins831@gmail.com>
2018-07-03 20:51:39 -0400

    Address & no results updated

---
+++ javascript/scriptSlider.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+
+  $('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 2000,
+  });
+
+});
